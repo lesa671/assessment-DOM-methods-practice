2b40baa7b8c4f2fc7bb83928118b629f428567af
Lesa Ducksworth <lducks67@gmail.com>
2020-06-16 17:09:23 -0500

    added target attribute to anchor element in javascript

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 // add an image and an anchor tag to tha main tag using js only -done!
 // add an image source to the image tag -done!
 // add a link to the anchor tag -done!
+// add event listener to button and remove <main> on click -done!
 
 const bodyElement = document.querySelector('body')
 const scriptElement = document.querySelector('script')
@@ -17,15 +18,19 @@ bodyElement.insertBefore(mainElement, scriptElement)
 let imageElement = document.createElement('img')
 imageElement.className = 'image'
 imageElement.src = 'https://d1doqjmisr497k.cloudfront.net/-/media/mccormick-us/recipes/mccormick/c/800/chocolate-almond-biscotti.jpg?vd=20180710T043943Z&hash=0F61B2A531CF290F939F86EA75031EA2A58FB584'
+imageElement.alt = 'picture of chocolate almond biscotti'
 imageElement.style.height = '500px'
 mainElement.append(imageElement)
+
 let anchorElement = document.createElement('a')
 anchorElement.className = 'link'
 anchorElement.append('Link to recipe for chocolate almond biscotti')
 anchorElement.href = 'https://www.mccormick.com/recipes/dessert/chocolate-almond-biscotti'
+anchorElement.target = '_blank'
 mainElement.append(anchorElement)
 
 buttonElement.addEventListener('click', function () {
     mainElement.remove()
     console.log('remove button clicked')
 })
+
